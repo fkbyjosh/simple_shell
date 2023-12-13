9fefee754535dec5fd696fa49984542e988a9ec5
emmanuelayettey <emmanuelayettey029@gmail.com>
2023-12-13 13:28:45 -0800

    Built In

--- shell.h
+++ shell.h
@@ -11,4 +11,12 @@ void constant_prompt(void);
 void _printf(const char *command);
 void exe_command(const char *command);
 void process_input(char *command, size_t size);
+
+/* sig_builtins1.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+
+
 #endif /* SHELL_H */
